7279331144153d93a9c3c8370242264206fdf7d6
englishclasseswcami-droid <englishclasseswcami@gmail.com>
2026-06-20 17:41:08 -0300

    Delete js directory

--- js/config.js
+++
@@ -1,6 +0,0 @@
-const TEACHER_EMAIL = 'englishclasseswcami@gmail.com';
-const SUPABASE_URL = 'https://flfyifdaitfnxncoxzxe.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsZnlpZmRhaXRmbnhuY294enhlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODA4Njc0NTQsImV4cCI6MjA5NjQ0MzQ1NH0.wKMJk0EIYi1hRwoE1EN5lfEGIn2c9YrJhPi_UO2w2gI';
-
-const sb = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
-
